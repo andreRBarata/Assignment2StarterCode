05d1ebb643ae6ba365c8b66ab01fe0d936cfe7ea
Andre Barata <andre.quina.barata@gmail.com>
2014-12-09 17:08:39 +0000

    Initial code merge

--- Assignment2StarterCode.pde
+++ Assignment2StarterCode.pde
@@ -1,83 +1,117 @@
 /*
-    DIT OOP Assignment 2 Starter Code
-    =================================
-    
-    Loads player properties from an xml file
-    See: https://github.com/skooter500/DT228-OOP 
+		DIT OOP Assignment 2 Starter Code
+		=================================
+		
+		Loads player properties from an xml file
+		See: https://github.com/skooter500/DT228-OOP 
 */
+import java.util.TreeMap;
 
-ArrayList<Player> players = new ArrayList<Player>();
+PVector[][] rectangle = {
+	{new PVector(0,0), new PVector(50,0)},
+	{new PVector(0,20), new PVector(50,20)},
+	{new PVector(50,0), new PVector(50,20)},
+	{new PVector(0,0), new PVector(0,20)}
+};
+
+ArrayList<Player> players;
+ArrayList<Button> buttons;
 boolean[] keys = new boolean[526];
 
-void setup()
-{
-  size(500, 500);
-  setUpPlayerControllers();
+void setup() {
+	players = new ArrayList<Player>();
+	buttons = new ArrayList<Button>();
+	
+	background(255);
+	size(500, 500);
+	setUpPlayerControllers();
+	
+	Button button = new Button(
+		new PVector(width/2, height/2),
+		"button",
+		rectangle,
+		new CallBack() {
+			public void run() {
+				println("teste");
+			}
+		}
+	);
+	
+	buttons.add(button);
 }
 
-void draw()
-{
-  for(Player player:players)
-  {
-    player.update();
-    player.display();
-  }
+void draw() {
+	background(255);
+	for(Player player: players) {
+		player.update();
+		player.draw();
+	}
+	
+	for (int i = 0; i < buttons.size(); i++) {
+		Button button = buttons.get(i);
+		button.draw();
+	}
 }
 
-void keyPressed()
-{
-  keys[keyCode] = true;
+void keyPressed() {
+	keys[keyCode] = true;
 }
 
-void keyReleased()
-{
-  keys[keyCode] = false;
+void keyReleased() {
+	keys[keyCode] = false;
 }
 
-boolean checkKey(char theKey)
-{
-  return keys[Character.toUpperCase(theKey)];
+boolean checkKey(char theKey) {
+	return keys[Character.toUpperCase(theKey)];
 }
 
-char buttonNameToKey(XML xml, String buttonName)
-{
-  String value =  xml.getChild(buttonName).getContent();
-  if ("LEFT".equalsIgnoreCase(value))
-  {
-    return LEFT;
-  }
-  if ("RIGHT".equalsIgnoreCase(value))
-  {
-    return RIGHT;
-  }
-  if ("UP".equalsIgnoreCase(value))
-  {
-    return UP;
-  }
-  if ("DOWN".equalsIgnoreCase(value))
-  {
-    return DOWN;
-  }
-  //.. Others to follow
-  return value.charAt(0);  
+char buttonNameToKey(XML xml, String buttonName) {
+	String value =  xml.getChild(buttonName).getContent();
+	if ("LEFT".equalsIgnoreCase(value)) {
+		return LEFT;
+	}
+	if ("RIGHT".equalsIgnoreCase(value)) {
+		return RIGHT;
+	}
+	if ("UP".equalsIgnoreCase(value)) {
+		return UP;
+	}
+	if ("DOWN".equalsIgnoreCase(value)) {
+		return DOWN;
+	}
+	//.. Others to follow
+	return value.charAt(0);  
 }
 
-void setUpPlayerControllers()
-{
-  XML xml = loadXML("arcade.xml");
-  XML[] children = xml.getChildren("player");
-  int gap = width / (children.length + 1);
-  
-  for(int i = 0 ; i < children.length ; i ++)  
-  {
-    XML playerXML = children[i];
-    Player p = new Player(
-            i
-            , color(random(0, 255), random(0, 255), random(0, 255))
-            , playerXML);
-    int x = (i + 1) * gap;
-    p.pos.x = x;
-    p.pos.y = 300;
-   players.add(p);         
-  }
+void setUpPlayerControllers() {
+	XML xml = loadXML("arcade.xml");
+	XML[] children = xml.getChildren("player");
+	int gap = width / (children.length + 1);
+	
+	for(int i = 0; i < children.length; i ++) {
+		XML playerXML = children[i];
+		Player p = new Player(
+			i,
+			color(random(0, 255), random(0, 255), random(0, 255)),
+			playerXML
+		);
+		int x = (i + 1) * gap;
+		p.position.x = x;
+		p.position.y = 300;
+		players.add(p);         
+	}
+}
+
+void mouseClicked() {
+	for (int i = 0; i < buttons.size(); i++) {
+		Button button = buttons.get(i);
+		
+		if (mouseX > button.position.x && mouseX < button.position.x + button.shape.maxWidth()) {
+			if (mouseY > button.position.y && mouseY < button.position.y + button.shape.maxHeight()) {
+				fill(color(50));
+				button.callback.run();
+				fill(color(0));
+			}
+		}
+	}
 }
---
+++ src/source/Assignment2StarterCode.java
@@ -0,0 +1,317 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.TreeMap; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Assignment2StarterCode extends PApplet {
+
+/*
+		DIT OOP Assignment 2 Starter Code
+		=================================
+		
+		Loads player properties from an xml file
+		See: https://github.com/skooter500/DT228-OOP 
+*/
+
+
+PVector[][] rectangle = {
+	{new PVector(0,0), new PVector(50,0)},
+	{new PVector(0,20), new PVector(50,20)},
+	{new PVector(50,0), new PVector(50,20)},
+	{new PVector(0,0), new PVector(0,20)}
+};
+
+ArrayList<Player> players;
+ArrayList<Button> buttons;
+boolean[] keys = new boolean[526];
+
+public void setup() {
+	players = new ArrayList<Player>();
+	buttons = new ArrayList<Button>();
+	
+	background(255);
+	size(500, 500);
+	setUpPlayerControllers();
+	
+	Button button = new Button(
+		new PVector(width/2, height/2),
+		"button",
+		rectangle,
+		new CallBack() {
+			public void run() {
+				println("teste");
+			}
+		}
+	);
+	
+	buttons.add(button);
+}
+
+public void draw() {
+	background(255);
+	for(Player player: players) {
+		player.update();
+		player.draw();
+	}
+	
+	for (int i = 0; i < buttons.size(); i++) {
+		Button button = buttons.get(i);
+		button.draw();
+	}
+}
+
+public void keyPressed() {
+	keys[keyCode] = true;
+}
+
+public void keyReleased() {
+	keys[keyCode] = false;
+}
+
+public boolean checkKey(char theKey) {
+	return keys[Character.toUpperCase(theKey)];
+}
+
+public char buttonNameToKey(XML xml, String buttonName) {
+	String value =  xml.getChild(buttonName).getContent();
+	if ("LEFT".equalsIgnoreCase(value)) {
+		return LEFT;
+	}
+	if ("RIGHT".equalsIgnoreCase(value)) {
+		return RIGHT;
+	}
+	if ("UP".equalsIgnoreCase(value)) {
+		return UP;
+	}
+	if ("DOWN".equalsIgnoreCase(value)) {
+		return DOWN;
+	}
+	//.. Others to follow
+	return value.charAt(0);  
+}
+
+public void setUpPlayerControllers() {
+	XML xml = loadXML("arcade.xml");
+	XML[] children = xml.getChildren("player");
+	int gap = width / (children.length + 1);
+	
+	for(int i = 0; i < children.length; i ++) {
+		XML playerXML = children[i];
+		Player p = new Player(
+			i,
+			color(random(0, 255), random(0, 255), random(0, 255)),
+			playerXML
+		);
+		int x = (i + 1) * gap;
+		p.position.x = x;
+		p.position.y = 300;
+		players.add(p);         
+	}
+}
+
+public void mouseClicked() {
+	for (int i = 0; i < buttons.size(); i++) {
+		Button button = buttons.get(i);
+		
+		if (mouseX > button.position.x && mouseX < button.position.x + button.shape.maxWidth()) {
+			if (mouseY > button.position.y && mouseY < button.position.y + button.shape.maxHeight()) {
+				fill(color(50));
+				button.callback.run();
+				fill(color(0));
+			}
+		}
+	}
+}
+interface CallBack {
+	public void run();
+}
+
+class Shape {
+	PVector[][] shape;
+	
+	Shape(PVector[][] shape) {
+		this.shape = shape;
+	}
+	
+	public int size() {
+		return shape.length;
+	}
+	
+	public PVector[] get(int i) {
+		return shape[i];
+	}
+	
+	public float maxWidth() {
+		float maxWidth = 0;
+		for (int i = 0; i < shape.length; i++) {
+			if (i == 0 || shape[i][0].x > maxWidth) {
+				maxWidth = shape[i][0].x;
+			}
+			if (i == 0 || shape[i][1].x > maxWidth) {
+				maxWidth = shape[i][1].x;
+			}
+		}
+		
+		return maxWidth;
+	}
+	
+	public float maxHeight() {
+		float maxHeight = 0;
+		for (int i = 0; i < shape.length; i++) {
+			if (i == 0 || shape[i][0].y > maxHeight) {
+				maxHeight = shape[i][0].y;
+			}
+			if (i == 0 || shape[i][1].y > maxHeight) {
+				maxHeight = shape[i][1].y;
+			}
+		}
+		
+		return maxHeight;
+	}
+}
+
+class Drawable {
+	PVector position;
+	Shape shape;
+	
+	Drawable(PVector position, PVector[][] shape) {
+		this.position = position;
+		this.shape = new Shape(shape);
+	}
+	
+	public void draw() {
+		for (int i = 0; i < shape.size(); i++) {
+			PVector current1 = PVector.add(shape.get(i)[0], position);
+			PVector current2 = PVector.add(shape.get(i)[1], position);
+			
+			line(
+				current1.x,
+				current1.y,
+				current2.x,
+				current2.y
+			);
+		}
+	}
+}
+
+class Button extends Drawable {
+	CallBack callback;
+	String text;
+	
+	Button(PVector position, String text, PVector[][] shape, CallBack callback) {
+		super(position, shape);
+		
+		
+		this.callback = callback;
+		this.text = text;
+	}
+	
+	public void draw() {
+		fill(color(255));
+
+		super.draw();
+		
+		fill(color(0));
+		
+		text(
+			text,
+			position.x + this.shape.maxWidth()/2 - textWidth(text)/2,
+			position.y + (this.shape.maxHeight() + 10)/2
+		);
+	}
+}
+
+class Player extends Drawable {
+	TreeMap<String, Character> keyBinds;
+	int index;
+	int colour;
+		
+	Player() {
+		super(
+			new PVector(width / 2, height / 2),
+			rectangle
+		);
+		this.keyBinds = new TreeMap<String, Character>();
+	}
+	
+	Player(int index, int colour) {
+		this();
+		this.index = index;
+		this.colour = colour;
+	}
+	
+	Player(int index, int colour, TreeMap keyBinds) {
+		this();
+		this.index = index;
+		this.colour = colour;
+		this.keyBinds = keyBinds;
+	}
+	
+	Player(int index, int colour, XML xml) {
+		this(
+			index,
+			colour
+		);
+
+		String[] keys = {
+			"up",
+			"down",
+			"left",
+			"right",
+			"start",
+			"button1",
+			"button2"
+		};
+		
+		for (String key: keys) {
+			this.keyBinds.put(
+				key,
+				buttonNameToKey(xml, key)
+			);
+		}
+	}
+	
+	public void update() {
+		if (checkKey(keyBinds.get("up"))) {
+		  position.y -= 1;
+		}
+		if (checkKey(keyBinds.get("down"))) {
+		  position.y += 1;
+		}
+		if (checkKey(keyBinds.get("left"))) {
+		  position.x -= 1;
+		}    
+		if (checkKey(keyBinds.get("right"))) {
+		  position.x += 1;
+		}
+		if (checkKey(keyBinds.get("start"))) {
+		  println("Player " + index + " start");
+		}
+		if (checkKey(keyBinds.get("button1"))) {
+		  println("Player " + index + " button 1");
+		}
+		if (checkKey(keyBinds.get("button2"))) {
+		  println("Player " + index + " butt2");
+		}    
+	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Assignment2StarterCode" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
